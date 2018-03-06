d010acd0293c061f34c00abad4c82551da7f2afe
Douglas Matsumoto <dmatsumoto@fitec.org.br>
2018-03-06 18:51:23 -0300

    finished implementation of overview chart

--- src/main/resources/static/assets/js/stats.js
+++ src/main/resources/static/assets/js/stats.js
@@ -14,38 +14,39 @@ function getFridgeOverview(){
 	$('#expired-number').html(expiredItems);
 	$('#exp-soon-number').html(expireSoon);
 	pieData = [expiredItems, expireSoon, stillGoodItems];
+	initOverviewChart();
 	})
 }
 
-
-
-var data = {
-	labels: ['Expired', 'Soon','Good'],
-    datasets: [{
-    	data: pieData,
-        //data: [expiredItems, expireSoon, stillGoodItems],
-    	labels: 'Fridge Overview',
-    	backgroundColor: [
-    		'rgba(235, 94, 40, 0.7)',
-        	'rgba(243, 187, 69, 0.7)',
-        	'rgba(122, 194, 154, 0.7)'
-    	],
-    	borderColor: [
-        	'rgba(235, 94, 40, 1)',
-        	'rgba(243, 187, 69, 1)',
-        	'rgba(122, 194, 154, 1)'
-    	]
-    }]
-};
-
 function initOverviewChart(){
-	console.log(pieData);
 	var ctx = document.getElementById("overviewChart");
 	var overviewChart = new Chart(ctx, {
     type: 'doughnut',
-    data: data
+    data: retrieveDataSet()
 	});
 }
+
+function retrieveDataSet(){
+	var dataSet = {
+		labels: ['Expired', 'Soon','Good'],
+	    datasets: [{
+	    	data: pieData,
+	    	labels: 'Fridge Overview',
+	    	backgroundColor: [
+	    		'rgba(235, 94, 40, 0.7)',
+	        	'rgba(243, 187, 69, 0.7)',
+	        	'rgba(122, 194, 154, 0.7)'
+	    	],
+	    	borderColor: [
+	        	'rgba(235, 94, 40, 1)',
+	        	'rgba(243, 187, 69, 1)',
+	        	'rgba(122, 194, 154, 1)'
+	    	]
+	    }]
+	}
+	return dataSet;
+}
+
 
 
 
